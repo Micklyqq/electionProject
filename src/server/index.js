773573a71e902973cecffb8c9314505c4b76e2ed
Micklyqq <maykiqq@gmail.com>
2024-06-02 23:31:58 +0300

    add cors

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const app = express();
+const cors = require('cors');
 const router = require("./routes/index");
 const errorHandler = require('./middleware/errorHandlingMiddleware')
 const DatabaseInteraction = require("./database/databaseInteraction");
 const PORT = 3001;
 
 const db = new DatabaseInteraction();
+const app = express();
+app.use(cors())
 app.use(express.json());
 app.use('/api',router);
 app.use(errorHandler);

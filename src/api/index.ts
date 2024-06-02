6b248d4f2e0a988495b080f3e2c9677630e03c75
Micklyqq <maykiqq@gmail.com>
2024-06-02 18:49:34 +0300

    create axios config

---
+++ src/api/index.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+const baseURL = "http://localhost:3001";
+const $host = axios.create({
+    baseURL:baseURL
+});
+
+const $authHost = axios.create({
+    baseURL:baseURL
+});
+
+const authIntercepter = (config:any)=>{
+    config.headers.authorization = `Bearer ${localStorage.getItem('token')}`;
+    return config;
+};
+
+$authHost.interceptors.request.use(authIntercepter);
+
+export{
+    $host,
+    $authHost
+};

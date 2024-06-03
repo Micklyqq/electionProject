92d37b8cf466b4fb4a808bb70ccb684f11fab3b0
Micklyqq <maykiqq@gmail.com>
2024-06-03 11:55:28 +0300

    add exit func

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -3,6 +3,10 @@ import { Container, Image, Nav, Navbar } from "react-bootstrap";
 import BelFlag from "../assets/img/bel-flag.png"
 import { useNavigate } from "react-router-dom";
 export default function NavigationBar(){
+    const exit = ()=>{
+        localStorage.removeItem('token');
+        navigate('/')
+    }
     const navigate = useNavigate();
             return (
             <>
@@ -13,7 +17,7 @@ export default function NavigationBar(){
                         <Nav.Link onClick={()=>navigate("/main")}>Главная</Nav.Link>
                         <Nav.Link>О сайте</Nav.Link>
                         <Nav.Link>Профиль</Nav.Link>
-                        <Nav.Link>Выход</Nav.Link>
+                        <Nav.Link onClick={()=>exit()} >Выход</Nav.Link>
                     </Nav>
                     </Container>
             </Navbar>

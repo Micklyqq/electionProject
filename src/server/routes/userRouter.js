48789e09b4b817a12b7f288bc09c06bfc52b4916
Micklyqq <maykiqq@gmail.com>
2024-06-03 11:59:22 +0300

    add checkAuth route

--- src/server/routes/userRouter.js
+++ src/server/routes/userRouter.js
@@ -1,9 +1,11 @@
 const Router = require("express");
 const router = new Router();
 const userController = require("../controllers/userController");
+const authMiddleware = require("../middleware/authMiddleware");
 
 router.post("/reg",userController.registration);
 router.post("/auth",userController.auth);
 router.post("/giveAdmin",userController.giveAdmin);
+router.get('/checkAuth',authMiddleware,userController.check)
 
 module.exports = router;
